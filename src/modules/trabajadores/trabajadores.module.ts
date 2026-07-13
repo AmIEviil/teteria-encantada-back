f5461a0565d5361a01a25397206b98d5ad68724c
GonzaloCaroK <gonzalo.caro@kabeli.cl>
2026-07-13 19:57:46 -0400

    feat(trabajadores): registro de horas trabajadas por dia

--- src/modules/trabajadores/trabajadores.module.ts
+++ src/modules/trabajadores/trabajadores.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from '../auth/entities/role.entity';
 import { User } from '../auth/entities/user.entity';
+import { RegistroHora } from './entities/registro-hora.entity';
 import { Trabajador } from './entities/trabajador.entity';
+import { RegistroHorasController } from './registro-horas.controller';
+import { RegistroHorasService } from './registro-horas.service';
 import { TrabajadoresController } from './trabajadores.controller';
 import { TrabajadoresService } from './trabajadores.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role, Trabajador])],
-  controllers: [TrabajadoresController],
-  providers: [TrabajadoresService],
+  imports: [TypeOrmModule.forFeature([User, Role, Trabajador, RegistroHora])],
+  controllers: [RegistroHorasController, TrabajadoresController],
+  providers: [TrabajadoresService, RegistroHorasService],
 })
 export class TrabajadoresModule {}
